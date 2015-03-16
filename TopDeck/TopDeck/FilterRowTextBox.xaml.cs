591d4148787722696e098a45ab0c4ccfd23a9d62
KyleHersey <herseykw@plu.edu>
2015-03-16 16:05:15 -0700

    Initial commit
    Added GUI elements. Not yet completed

---
+++ TopDeck/TopDeck/FilterRowTextBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TopDeck
+{
+    /// <summary>
+    /// Interaction logic for FilterRowTextBox.xaml
+    /// </summary>
+    public partial class FilterRowTextBox : UserControl
+    {
+
+        public string FilterText
+        {
+            get;
+            set;
+        }
+
+        public FilterRowTextBox()
+        {
+            InitializeComponent();
+            FilterType.DataContext = this;
+        }
+    }
+}
---
+++ TopDeck/TopDeck/FiltersPanel.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TopDeck
+{
+    /// <summary>
+    /// Interaction logic for FiltersPanel.xaml
+    /// </summary>
+    public partial class FiltersPanel : UserControl
+    {
+
+        public List<String> RarityList
+        {
+            get{
+                List<String> tempList = new List<String>();
+                tempList.Add("Rarity");
+                tempList.Add("Rare");
+                tempList.Add("Uncommon");
+                tempList.Add("Common");
+
+                return tempList;
+            }
+        }
+
+        public FiltersPanel()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void TypeFilterField_KeyUp(object sender, KeyEventArgs e)
+        {
+            FilterRowTextBox sentBy = (FilterRowTextBox)sender;
+                if (sentBy.Input.Text == "Creature")
+                {
+                    PowerFilterField.Visibility = System.Windows.Visibility.Visible;
+                    ToughnessFilterField.Visibility = System.Windows.Visibility.Visible;
+                }
+                else
+                {
+                    PowerFilterField.Visibility = System.Windows.Visibility.Collapsed;
+                    ToughnessFilterField.Visibility = System.Windows.Visibility.Collapsed;
+                }
+        }
+    }
+}
