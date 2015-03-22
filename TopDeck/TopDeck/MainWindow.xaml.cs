08f6d9fbf242fc31cb2f18678a6e534463c4ff1c
Emerald Baldwin <baldwier@plu.edu>
2015-03-22 15:20:16 -0700

    Add SaveAs click ability to file dropdown
    Currently does not actually save the file.

--- TopDeck/TopDeck/MainWindow.xaml.cs
+++ TopDeck/TopDeck/MainWindow.xaml.cs
@@ -63,5 +63,29 @@ namespace TopDeck
                 Debug.WriteLine("yes");
             }
         }
+
+        private void SaveAsDeckFile_Click(object sender, RoutedEventArgs e)
+        {
+            /*Process fileExplorer = Process.Start("explorer.exe"); */
+            string path = "";
+            SaveFileDialog file = new SaveFileDialog();
+            file.OverwritePrompt = true;
+
+            // Set the file name to myText.txt, set the type filter 
+            // to text files, and set the initial directory to the  
+            // MyDocuments folder.
+            file.FileName = "myDeck";
+            // DefaultExt is only used when "All files" is selected from  
+            // the filter box and no extension is specified by the user.
+            file.DefaultExt = "dec";
+            file.Filter =
+                "Deck files (*.dec)|*.dec|All files (*.*)|*.*";
+            bool? userClickedOK = file.ShowDialog();
+            if (userClickedOK == true)
+            {
+                path = file.SafeFileName;
+                Debug.WriteLine(path);
+            }
+        }
     }
 }
