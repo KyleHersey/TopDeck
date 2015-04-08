da9f587dfa1c4e960bbc0fa6af40533e2f5f47cb
KyleHersey <herseykw@plu.edu>
2015-04-07 19:10:21 -0700

    Removed 'RarityList' from filters panel (artifact from previous binding attempt)

--- TopDeck/TopDeck/FiltersPanel.xaml.cs
+++ TopDeck/TopDeck/FiltersPanel.xaml.cs
@@ -34,19 +34,6 @@ namespace TopDeck
             set;
         }
 
-        public List<String> RarityList
-        {
-            get{
-                List<String> tempList = new List<String>();
-                tempList.Add("Rarity");
-                tempList.Add("Rare");
-                tempList.Add("Uncommon");
-                tempList.Add("Common");
-
-                return tempList;
-            }
-        }
-
         public FiltersPanel()
         {
             InitializeComponent();
@@ -85,7 +72,30 @@ namespace TopDeck
                 requireMulticolor = true;
             }
             // what about colorless?
-            MiddlePanel.setItemsSource(DBMan.GetCards("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", null, requireMulticolor, colors, "", ""));
+            MiddlePanel.setItemsSource(DBMan.GetCards(
+                NameFilterField.Input.Text,                 //Name
+                ToughnessFilterField.Input.Text,            //Toughness
+                "",                                         //Hand
+                ConvertedManaCostFilterField.Input.Text,    //Converted Mana Cost
+                "",                                         //Release Date
+                "",                                         //Border
+                "",                                         //Watermark
+                "",                                         //Multiverse ID
+                LoyaltyFilterField.Input.Text,              //Loyalty
+                "",                                         //Rarity
+                "",                                         //Flavor
+                "",                                         //Artist
+                "",                                         //Number
+                "",                                         //Power
+                "",                                         //Life
+                "",                                         //Image Name
+                Text.Input.Text,                            //Card Text
+                "",                                         //Types
+                null,                                       //Timeshifted
+                requireMulticolor,                          //Require Multicolored
+                colors,                                     //Colors
+                "",                                         //subtype
+                ""));                                       //supertype
         }
 
         public void setDatabaseManager(DatabaseManager db)
