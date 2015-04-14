365da86c86dd05416f836ab629a1c33ddf8fe447
KyleHersey <herseykw@plu.edu>
2015-04-13 17:44:04 -0700

    Added functionality to parsing deck files
    Can now parse deck files formatted with # cardName \n
    Added two sample deck files formatted in this way.

--- TopDeck/TopDeck/DecklistManager.cs
+++ TopDeck/TopDeck/DecklistManager.cs
@@ -26,8 +26,48 @@ namespace TopDeck
             {
                 String tempString = input.ReadLine();
                 string[] words = tempString.Split(' ');
-                int[] nums = new int[words.Length];
+               // int[] nums = new int[words.Length];           used in previous parsing method
+                int multiverseID = -1;
 
+                if (words.Length == 2)   //if there's a possibility we have a multiverse id...
+                {
+                    try
+                    {
+                        multiverseID = Convert.ToInt32(words[1]);
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            multiverseID = Convert.ToInt32(DBMan.GetHighestMultiverseId(words[1]));
+                        }
+                        catch
+                        {
+                            multiverseID = -1;
+                        }
+                    }
+                }
+
+                if (multiverseID != -1) //if we got a multiverseID
+                {
+                    cardNames.Add(new Tuple<int, string, string>(Convert.ToInt32(words[0]), DBMan.GetAName(words[1].ToString()), Convert.ToString(words[1])));
+                }
+                else //if we didn't, assume #, cardname
+                {
+                    int num = Convert.ToInt32(words[0]);
+                    string name = words[1];
+                    for (int i = 2; i < words.Length; i++)
+                    {
+                        name += " ";
+                        name += words[i];
+                    }
+                    cardNames.Add(new Tuple<int, string, string>(num, name, DBMan.GetHighestMultiverseId(name)));
+                }
+
+
+                /*
+                 * previous parsing method
+                 * 
                 //fill nums
                 for (int i = 0; i < words.Length; i++)
                 {
@@ -35,7 +75,7 @@ namespace TopDeck
                 }
 
                 cardNames.Add(new Tuple<int, string, string>(nums[0], DBMan.GetAName(nums[1].ToString()), nums[1].ToString()));
-
+                */
             }
 
             return cardNames;
