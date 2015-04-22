4d5fb96e29454ecaa7b0cbe07447cb39e67ebf3c
KyleHersey <herseykw@plu.edu>
2015-04-22 00:23:13 -0700

    Updated Stats panel
    Updated to include two more charts

--- TopDeck/TopDeck/StatsPanel.xaml.cs
+++ TopDeck/TopDeck/StatsPanel.xaml.cs
@@ -38,25 +38,99 @@ namespace TopDeck
             get;
             set;
         }
+
+        public ObservableCollection<KeyValuePair<string, int>> TypeSource
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<KeyValuePair<int, int>> CostSource
+        {
+            get;
+            set;
+        }
         
         public StatsPanel()
         {
             InitializeComponent();
 
-            ColorSource = new ObservableCollection<KeyValuePair<string, int>>{
-                new KeyValuePair<string, int>("White", 1),
-                new KeyValuePair<string, int>("Blue", 2),
-                new KeyValuePair<string, int>("Black", 1),
-                new KeyValuePair<string, int>("Red", 8),
-                new KeyValuePair<string, int>("Green", 16)
-            };
+            ColorSource = new ObservableCollection<KeyValuePair<string, int>>();
+            TypeSource = new ObservableCollection<KeyValuePair<string, int>>();
+            CostSource = new ObservableCollection<KeyValuePair<int, int>>();
 
+            TypesPie.DataContext = this;
             ColorPie.DataContext = this;
+            CostBar.DataContext = this;
         }
 
         public void updateStats(){
             updateColorChart();
+            updateTypesChart();
+            updateCostChart();
             Console.WriteLine("updateStats was called");
+        }
+
+        public void updateCostChart()
+        {
+            while (CostSource.Count > 0)
+            {
+                CostSource.RemoveAt(0);
+            }
+
+            Dictionary<int, int> costDict = new Dictionary<int, int>();
+
+            foreach (LocalTuple tup in deck)
+            {
+                Card card = DBMan.GetACardFromMultiverseId(tup.MultiverseId);
+                int intCost = Convert.ToInt32(card.CMC);
+                if (costDict.Keys.Contains(intCost))
+                {
+                    costDict[intCost]++;
+                }
+                else
+                {
+                    costDict.Add(intCost, 1);
+                }
+            }
+
+            List<int> costs = costDict.Keys.ToList<int>();
+            costs.Sort();
+
+            foreach (int cost in costs)
+            {
+                CostSource.Add(new KeyValuePair<int, int>(cost, costDict[cost]));
+            }
+        }
+
+        public void updateTypesChart(){
+            while (TypeSource.Count > 0)
+            {
+                TypeSource.RemoveAt(0);
+            }
+
+            Dictionary<String, int> typeDict = new Dictionary<string, int>();
+
+            foreach (LocalTuple tup in deck)
+            {
+                Card card = DBMan.GetACardFromMultiverseId(tup.MultiverseId);
+                foreach (String type in card.Types)
+                {
+                    if (typeDict.Keys.Contains(type))
+                    {
+                        typeDict[type]++;
+                    }
+                    else
+                    {
+                        typeDict.Add(type, 1);
+                    }
+                }
+            }
+
+            foreach (String type in typeDict.Keys)
+            {
+                TypeSource.Add(new KeyValuePair<string, int>(type, typeDict[type]));
+            }
         }
 
         public void updateColorChart()
@@ -88,7 +162,6 @@ namespace TopDeck
             }
 
             //go through dictionary
-            ObservableCollection<KeyValuePair<String, int>> newColors = new ObservableCollection<KeyValuePair<string, int>>();
             foreach (String color in colorDict.Keys)
             {
                 if (colorDict[color] > 0)
