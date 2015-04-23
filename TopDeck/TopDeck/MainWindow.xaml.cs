a100dda7d2df166b1e56eb0a418e2491ea14eb38
Emerald Baldwin <baldwier@plu.edu>
2015-04-23 16:41:17 -0700

    Add Light Theme
    Light Theme is not complete or currently selectable

--- TopDeck/TopDeck/MainWindow.xaml.cs
+++ TopDeck/TopDeck/MainWindow.xaml.cs
@@ -396,6 +396,7 @@ namespace TopDeck
             DeckTab.CardList.setItemsSource();
             DeckTab.CardList.Sideboard = sideboard;
             DeckTab.CardList.setSideboardItemsSource();
+            DeckTab.DeckStats.deck = currentDeck;
             DeckTab.DeckStats.updateStats();
             DeckTab.CardView.Clear();
             HideOrShowProxiesButton();
